a449576bf850ccb13bdf9e1040666ba3b2664da1
AMAF04 <ana.acunaf04@gmail.com>
2024-07-06 08:43:49 -0500

    Actualizacion de app

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ def obtener_productos():
         return "Error al obtener los productos", 500
 
 if __name__ == '__main__':
-    app.run(port=4000, host="0.0.0.0",debug=True)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host='0.0.0.0', port=port)
