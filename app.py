ab76a86d9be8c8e845e0aa19d7bf1239abd70b42
AMAF04 <ana.acunaf04@gmail.com>
2024-07-06 07:39:17 -0500

    Primer commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def obtener_productos():
+    url = 'https://fakestoreapi.com/products'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        productos = response.json()
+        return render_template('index.html', productos=productos)
+    else:
+        return "Error al obtener los productos", 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
